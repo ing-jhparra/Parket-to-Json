9c58ad2d73e95f3be53737a12b3cb38e7ce3eb78
Jesus Parra <parra.jesus@gmail.com>
2024-04-12 08:57:56 -0400

    Carga de la aplicacion

---
+++ parquet2_json.py
@@ -0,0 +1,39 @@
+import pandas as pd
+
+# Definicon de Rutas y nombre de archvios
+input_path = 'C:\\Users\\Jesus\\Documents\\proyectos\\Parket-to-Json\\input\\'
+output_path = 'C:\\Users\\Jesus\\Documents\\proyectos\\Parket-to-Json\\output\\'
+input_file = 'business.parquet'
+output_file = 'business.json'
+
+archivo_json = open (output_path + output_file,'w')
+
+def convertir_clave_valor(tupla):
+    clave, valor = tupla
+    if type(valor) == 
+    return f"'{clave}':'{valor}'"
+
+# Lee el archvio parquet y lo convierte en un DataFrame
+df = pd.read_parquet(input_path + input_file)
+columnas = df.columns
+
+# Construccion de una lista de colecciones a partid del DataFrame
+colecciones = list()
+for fila in range(len(df)):
+    colecciones.append([convertir_clave_valor((columnas[columna], str(df.iloc[fila,columna]))) for columna in range(len(columnas))])
+
+# Construccion del archivo json
+archivo_json.write("[  \r")
+for indice, coleccion in enumerate(colecciones):
+    archivo_json.write("  {\r")
+    for columna in range(len(coleccion)):
+        if columna == len(coleccion) - 1:
+            archivo_json.write("   " + coleccion[columna] + "\r")
+        else :
+            archivo_json.write("   " + coleccion[columna] + ",\r")
+    if indice == len(colecciones) - 1:
+        archivo_json.write("  }\r")
+    else :
+        archivo_json.write("  },\r")
+archivo_json.write("]  \r")
+archivo_json.close()
